d828df3deda10d541c1f4a8a662da843dfaa4de2
chamip <chamip@yeah.net>
2023-01-26 14:47:04 +0800

    学习lua配置nvim

---
+++ lua/grammer.lua
@@ -0,0 +1,36 @@
+--[[
+Author: chamip
+Date: 2023-01-26 13:25:38
+LastEditTime: 2023-01-26 13:47:46
+LastEditors: chamip
+FilePath: /lua/grammer.lua
+--]]
+
+-- 交互式编程
+-- lua
+-- lua -i
+
+-- 脚本式编程
+-- lua *.lua
+
+-- 指定解释器
+#!/usr/local/bin/lua
+print("helloworld!")
+
+-- 单行注释
+--[[
+    多行注释
+    多行注释
+--]]
+
+-- 标示符
+
+--[[
+    关键字
+    一般约定，以下划线开头连接一串大写字母的名字（比如 _VERSION）被保留用于 Lua 内部全局变量。
+--]]
+
+--[[
+    全局变量
+    当且仅当一个变量不等于nil时，这个变量即存在。
+--]]
