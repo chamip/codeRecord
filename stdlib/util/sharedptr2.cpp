a583696d63138d98278ec3a01eb6f57ad4a861ea
chamip <chamip@yeah.net>
2023-10-05 13:41:34 +0800

    shared_ptr

---
+++ stdlib/util/sharedptr2.cpp
@@ -0,0 +1,35 @@
+#include <string>
+#include <fstream>   // for ofstream
+#include <memory>    // for shared_ptr
+#include <cstdio>    // for remove()
+#include <iostream>
+
+class FileDeleter
+{
+  private:
+    std::string filename;
+  public:
+    FileDeleter (const std::string& fn)
+     : filename(fn) {
+        std::cout << "constructor." << std::endl;
+    }
+    void operator () (std::ofstream* fp) {
+        std::cout << "delete file." << std::endl;
+        delete fp;                     // close file
+        std::remove(filename.c_str()); // delete file
+    }
+    ~FileDeleter() {
+        std::cout << "destructor." << std::endl;
+    }
+};
+
+int main()
+{
+    // create and open temporary file:
+    std::shared_ptr<std::ofstream> fp(new std::ofstream("tmpfile.txt"),
+                                      FileDeleter("tmpfile.txt"));
+    //...
+    std::cout << "!!!!!" << std::endl;
+
+    //TODO: why call destructor 7 times.
+}
